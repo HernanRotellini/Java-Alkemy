fb5a99417d7e5bb5702b7cb0a744614601f8575e
HernanRotellini <hernanrotellini@hotmail.com>
2022-05-16 10:27:09 -0300

    Inicio proyecto
    1er commit inicio proyecto

---
+++ Alkemy/src/test/java/Alkemy/AlkemyApplicationTests.java
@@ -0,0 +1,13 @@
+package Alkemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
